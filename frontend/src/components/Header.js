3695a8b909f9ca5a84db8c20523321769e36f0a4
Iskender-Garajayev <yoloten.isa@gmail.com>
2022-08-07 05:46:04 +0300

    product component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,23 +1,23 @@
 import React from "react";
 import { Nav, Navbar, Container } from "react-bootstrap";
-import {LinkContainer} from 'react-router-bootstrap' 
+// import {LinkContainer} from 'react-router-bootstrap' 
 
 const Header = () => {
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <LinkContainer to='/' exact>
+          {/* <LinkContainer to='/' exact> */}
             <Navbar.Brand href="/">ProShop</Navbar.Brand>
-          </LinkContainer>
+          {/* </LinkContainer> */}
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-            <LinkContainer to='/cart'>
-              <Nav.Link >
+            {/* <LinkContainer to='/cart'> */}
+              <Nav.Link href="/cart" >
                 <i className="fas fa-shopping-cart"> </i> Cart
               </Nav.Link>
-              </LinkContainer>
+              {/* </LinkContainer> */}
               <Nav.Link href="/login">
                 <i className="fas fa-user"></i> Sing In
               </Nav.Link>
