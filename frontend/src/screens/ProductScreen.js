94f9dde9b07de32a121ec017d85aac29b1ea31fa
Iskender-Garajayev <yoloten.isa@gmail.com>
2022-08-09 06:54:55 +0300

    update

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -8,16 +8,15 @@ import axios from 'axios'
 const ProductScreen = ({match}) => {
 
   const [product, setProduct] = useState({})
+  
 
-  useEffect(() =>{
-    const fetchProduct = async () => {
-      const {data} = await axios.get(`/api/products/${match.params.id}`)
-
-      setProduct(data)
+  useEffect(() => {
+    const fethcProduct = async () => {
+      const {data} = await axios.get(`/api/product/${match.params.id}`)
+      setProduct(data) 
     }
-    fetchProduct()
-  }, [])
-
+    fethcProduct()
+  },[])
 
   return (
     <>
